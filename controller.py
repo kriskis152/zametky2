898626da6a304cc1b16a0718e2fdfb495684216e
bobrdobr1z <bobrdobr112zxc@bk.ru>
2025-12-01 21:22:35 +0300

    2 variant

---
+++ controller.py
@@ -0,0 +1,31 @@
+from loguru import logger
+
+
+class Controller:
+    def __init__(self):
+        self.action = ""
+    
+    def run(self, action):
+        logger.info(f"Контроллер запущен. Пользователь выбрал: {action}")
+        self.action = action
+        match action:
+            case 1:
+                self.create_note()
+            case 2:
+                self.update_note()
+            case 3:
+                self.delete_notes()
+            case 4:
+                self.show_all_notes()
+    
+    def create_note(self):
+        pass
+     
+    def update_note(self):
+        pass
+    
+    def delete_notes(self):
+        pass
+
+    def show_all_notes(self):
+        pass
---
+++ view.py
@@ -0,0 +1,15 @@
+from loguru import logger
+
+
+class View:
+    def __init__(self):
+        self.action = ""
+        
+    def start_menu(self):
+        logger.info("Пользовательское меню вызвано")
+        print("Я-приложение заметки.")
+        print("1. Создать заметку\n",
+              "2. Редактировать заметку\n",
+              "3. Удалить заметку\n")
+        action = int(input("Введите вариант: "))
+        return action
